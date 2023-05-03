b5ab6ab4b62bd2f96b841718ca99cc84ea5917c2
rlagkswn00 <hjkim2902@naver.com>
2023-05-03 17:36:42 +0900

    feat: app.js를 통해 더미 데이터 전송 가능

--- app.js
+++ app.js
@@ -1,28 +1,64 @@
-var express = require("express");
-var router = express.Router();
-var sensorLib = require("node-dht-sensor");
-
-var sensor = {
-  initialize: function () {
-    return sensorLib.initialize(11, 24);
-  },
-  read: function () {
-    var readout = sensorLib.read();
-    setTimeout(function () {
-      sensor.read();
-    }, 1500);
-
-    return readout;
-  },
+// var sensorLib = require("node-dht-sensor");
+const mqtt = require("mqtt");
+const options = {
+  host: "3.35.41.124", // 브로커 주소
+  port: 1883, // 포트
 };
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  var readout;
-  if (sensor.initialize()) {
-    readout = sensor.read();
-  }
-  res.setHeader("Content-Type", "application/json");
-  res.json(readout);
+const client = mqtt.connect(options);
+const template = {
+  time: "",
+  temperature: "",
+  humidity: "",
+  illuminance: "",
+};
+
+// var sensor = {
+//   initialize: function () {
+//     return sensorLib.initialize(11, 4);
+//   },
+//   read: function () {
+//     var readout = sensorLib.read();
+//     console.log(
+//       "Temperature: " +
+//         readout.temperature.toFixed(2) +
+//         "C, humidity: " +
+//         readout.humidity.toFixed(2) +
+//         "%" +
+//         readout.illuminance.toFixed(2)
+//     );
+//     setTimeout(function () {
+//       sensor.read();
+//     }, 1500);
+//   },
+// };
+var templateSetting = function () {
+  template.time = new Date();
+  template.humidity = "27";
+  template.temperature = "254";
+  template.illuminance = "22";
+
+  // template.humidity = readout.humidity.toFixed(2);
+  // template.temperature = readout.temperature.toFixed(2);
+  // template.illuminance = readout.illuminance.toFixed(2);
+};
+
+//구독 시 받는 데이터
+client.subscribe("test");
+client.on("message", function (topic, message) {
+  console.log(`토픽:${topic.toString()}, 메세지:${message.toString()}`);
 });
-module.exports = router;
+setInterval(() => {
+  // if (sensor.initialize()) {
+  //   sensor.read();
+  //   templateSetting();
+  // } else {
+  //   console.warn("Failed to initialize sensor");
+  // }
+
+  templateSetting();
+  var templateJSON = JSON.stringify(template);
+
+  client.publish("test", templateJSON, { qos: 2 });
+  console.log("send complete " + templateJSON);
+}, 2000);
