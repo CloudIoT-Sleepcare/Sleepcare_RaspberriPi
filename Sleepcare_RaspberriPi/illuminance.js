0f4f0b3a1b2362b2300b42332226615a5aa75063
rlagkswn00 <hjkim2902@naver.com>
2023-05-09 04:02:06 +0900

    feat: 조도센서 코드 추가

--- Sleepcare_RaspberriPi/illuminance.js
+++ Sleepcare_RaspberriPi/illuminance.js
@@ -1,9 +1,40 @@
-const gpio = require("node-wiring-pi");
-const LIGHT = 7;
+const Gpio = require("pigpio").Gpio;
+const Spi = require("spi-device");
 
-const CheckLight = function () {
-  let data = gpio.digitalRead(LIGHT);
-  console.log(data);
-  setTimeout(CheckLight, 200);
-};
-setTimeout(CheckLight, 200);
+const mcp3008 = Spi.openSync(0, 0, {
+  mode: Spi.MODE0,
+  maxSpeedHz: 1350000,
+});
+
+const adcChannel = 0;
+const VCC = 3.3;
+
+function readMcp3008(channel) {
+  const message = [
+    {
+      sendBuffer: Buffer.from([1, (8 + channel) << 4, 0]),
+      receiveBuffer: Buffer.alloc(3),
+      byteLength: 3,
+      speedHz: 1350000,
+    },
+  ];
+
+  mcp3008.transferSync(message);
+
+  const rawValue =
+    ((message[0].receiveBuffer[1] & 3) << 8) + message[0].receiveBuffer[2];
+
+  return (VCC * rawValue) / 1023;
+}
+
+const cds5Pin = new Gpio(17, {
+  mode: Gpio.INPUT,
+  pullUpDown: Gpio.PUD_UP,
+  edge: Gpio.FALLING_EDGE,
+  alert: true,
+});
+
+cds5Pin.on("alert", (level) => {
+  const cds5Value = readMcp3008(adcChannel);
+  console.log("CDS-5 value:", cds5Value);
+});
