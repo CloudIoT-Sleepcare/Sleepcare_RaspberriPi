3bc312491beb7729ae1036029c44c33f1969188c
rlagkswn00 <hjkim2902@naver.com>
2023-06-03 19:54:47 +0900

    feat: 새 cert 파일로 코드 수정

--- Sleepcare_RaspberriPi/aws-mqttClient.js
+++ Sleepcare_RaspberriPi/aws-mqttClient.js
@@ -2,29 +2,33 @@ const awsIot = require("aws-iot-device-sdk");
 const fs = require("fs");
 
 // AWS IoT Core 연결 정보
-const iotEndpoint = "aw8j5acz8wdeu-ats.iot.ap-northeast-2.amazonaws.com:8443"; // AWS IoT Core 엔드포인트 주소
 const certPath = "cert/";
+const iotEndpoint = "aw8j5acz8wdeu-ats.iot.ap-northeast-2.amazonaws.com"; // AWS IoT Core 엔드포인트 주소
 const privateKeyPath = certPath + "SleepCare.private.key"; // 라즈베리 파이의 개인 키 경로
 const clientCertificatePath = certPath + "SleepCare.cert.pem"; // 라즈베리 파이의 인증서 경로
 const caCertificatePath = certPath + "root-CA.crt"; // Root CA 인증서 경로
 const clientId = "Hanju Kim"; // 클라이언트 ID (원하는 값으로 변경)
 
+const privateKeyPath2 = certPath + "private.pem.key"; // 라즈베리 파이의 개인 키 경로
+const clientCertificatePath2 = certPath + "certificate.pem.crt"; // 라즈베리 파이의 인증서 경로
+const caCertificatePath2 = certPath + "AmazonRootCA1.pem"; // Root CA 인증서 경로
+
 // MQTT 클라이언트 초기화
 const device = awsIot.device({
-  privateKey: fs.readFileSync(privateKeyPath),
-  clientCert: fs.readFileSync(clientCertificatePath),
-  caCert: fs.readFileSync(caCertificatePath),
+  privateKey: fs.readFileSync(privateKeyPath2),
+  clientCert: fs.readFileSync(clientCertificatePath2),
+  caCert: fs.readFileSync(caCertificatePath2),
   clientId: clientId,
-  host: iotEndpoint
+  host: iotEndpoint,
 });
 
 // MQTT 연결 완료 이벤트 핸들러
 device.on("connect", () => {
   console.log("Connected to AWS IoT Core");
   device.subscribe("test", (err, granted) => {
-      err && console.log(err);
-      granted && console.log(granted)
-    })
+    err && console.log(err);
+    granted && console.log(granted);
+  });
 
   // 메시지 게시
   const topic = "test"; // 게시할 토픽 (원하는 값으로 변경)
@@ -42,4 +46,3 @@ device.on("error", (error) => {
 device.on("close", () => {
   console.log("Connection to AWS IoT Core closed");
 });
-
